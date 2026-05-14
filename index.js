7d1446004105f91c773adf17c41af7de7ec6a7ea
lllrjs <juannnst83@gmail.com>
2026-05-14 13:10:18 -0300

    Update index.js

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ async function gerarWrap(buffers, output = "wrap.jpg") {
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
-        headless: true,
+        headless: "new",
         executablePath: '/snap/bin/chromium',
         args: [
             "--no-sandbox",
