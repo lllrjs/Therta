3d185ef76f137b3bf6da79c06e8a193bf19e3034
lllrjs <juannnst83@gmail.com>
2026-05-14 23:56:05 -0300

    Update index.js

--- index.js
+++ index.js
@@ -98,7 +98,6 @@ const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
     headless: true,
-    executablePath: '/usr/bin/google-chrome-stable',
     args: [
         "--no-sandbox",
         "--disable-setuid-sandbox",
