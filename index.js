e6ff9a2b6be1e1860d6c74f29ff9d722beb0a5cc
lllrjs <juannnst83@gmail.com>
2026-05-16 14:17:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -522,14 +522,14 @@ if (comando.startsWith("!fm topmusicas")) {
 
 const qtdInput = parseInt(comando.split(" ")[2]) || 10;
 
-const qtd = Math.min(qtdInput, 30);
+const qtd = Math.min(qtdInput, 300);
         
         const url = `http://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&user=${username}&api_key=${process.env.LASTFM_API_KEY}&format=json&period=7day&limit=${qtd}`;
         const { data } = await axios.get(url);
 
         let txt = "🔥 top musicas:\n\n";
 
-        const tracks = data.topalbums.track;
+        const tracks = data.toptracks.track;
 
         tracks.forEach((t, i) => {
             txt += `${i + 1}. ${t.artist.name} - ${t.name}\n`;
@@ -589,7 +589,7 @@ const qtd = Math.min(qtdInput, 30);
     // =========================
     if (comando === "!fm topalbuns") {
     try {
-        const url = `http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=${username}&api_key=${process.env.LASTFM_API_KEY}&format=json&period=7day&limit=10`;
+        const url = `http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=${username}&api_key=${process.env.LASTFM_API_KEY}&format=json&period=7day&limit=12`;
 
         const { data } = await axios.get(url);
 
